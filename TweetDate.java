a32209b941313b0d04d5d3d68822f4b8de2d6df7
polsson12 <philip.olsson12@gmail.com>
2015-04-12 22:37:40 +0200

    fetching tweets on several pages.
    fetching tweets on a specified number of pages. Its controlled by the
    variable ”iter”.

---
+++ TweetDate.java
@@ -0,0 +1,11 @@
+
+
+
+
+import java.util.Date;
+
+public class TweetDate {
+    int numOfTweets;
+    Date date;
+}
+		    
--- TweetsExtractor.java
+++ TweetsExtractor.java
@@ -13,11 +13,13 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.*;
+import java.util.ArrayList;
 
 
-public class TweetsExtractor{
+public class TweetsExtractor {
     
     Twitter twitter;
+    ArrayList<TweetDate> tweetListDate = new ArrayList<TweetDate>();
     
     
     
@@ -26,30 +28,58 @@ public class TweetsExtractor{
     }
     
     public void search(){
+        int iter = 0;
         try {
             
             //Query query = new Query("@gunthermarder"); //referencing person “gunthermarder”.
-            Query query = new Query("@from:gunthermarder"); //sent from person “gunthermarder”.
+            Query query = new Query("#svpol");
             query.setCount(100); //Specifies the maximum number of responses per page, (100 is max)
-            QueryResult result = twitter.search(query);
-            System.out.println("Result size: " + result.getCount());
+            QueryResult result;
+            do {
+                result = twitter.search(query);
+                //System.out.println("Result size: " + result.getCount());
+                
+                for (Status status : result.getTweets()) {
+                    //System.out.println("@" + status.getUser().getScreenName() + ":" +  status.getText() + " ");
+                    
+                    if(!tweetListDate.isEmpty()){
+                        if(tweetListDate.get(tweetListDate.size()-1).date == status.getCreatedAt() ) { //Increment num of tweets for this date
+                            tweetListDate.get(tweetListDate.size() - 1).numOfTweets++;
+                        } else {
+                            TweetDate d = new TweetDate();
+                            d.numOfTweets = 1;
+                            d.date = status.getCreatedAt();
+                            tweetListDate.add(d);
+                        }
+                    } else {    //insert the first element
+                        TweetDate d = new TweetDate();
+                        d.numOfTweets = 1;
+                        d.date = status.getCreatedAt();
+                        tweetListDate.add(d);
+                    }
+                }
+                iter++;
+            } while ((query = result.nextQuery()) != null && iter < 300);  //Set iter to a max limit to avoid exceeding the rate limit
             
-            for (Status status : result.getTweets()) {
-                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-            }
+            
+            System.out.println("list size: " + tweetListDate.size());
+            for (int i = 0; i < tweetListDate.size(); i++)
+                System.out.println("Date:" + tweetListDate.get(i).date + " " + tweetListDate.get(i).numOfTweets);
             
         }
         catch (TwitterException te) {
             System.out.println("Couldn't connect: " + te);
         }
-    
+        
     }
+    
+    
     
 
     public static void main(String[] args) {
         
         TweetsExtractor s = new TweetsExtractor();
-        ts.search();
+        s.search();
         
     }
 
